69137359d1423a93a001cbfc768286e1a24dfae6
Michael Matloka <dev@twixes.com>
2020-11-26 08:15:44 +0100

    Add integrated ingestion server

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,16 @@ import yargs from 'yargs'
 
 yargs
     .scriptName('posthog-plugins')
-    .option('config', { alias: 'c', describe: 'json string of config options', type: 'string' })
-    .command(['start', '$0'], 'start the server', ({ argv }) => {
-        startPluginsServer(argv.config ? JSON.parse(argv.config) : {})
+    .option('config', { alias: 'c', describe: 'Config options JSON.', type: 'string' })
+    .option('ingest', { alias: 'i', describe: 'Whether ingestion server should be started.', type: 'boolean' })
+    .option('ingestionPort', { alias: 'p', describe: 'Ingestion server port.', type: 'number' })
+    .option('ingestionHostname', { alias: 'h', describe: 'Ingestion server hostname.', type: 'string' })
+    .command(['start', '$0'], 'start the server', ({ argv: { config, ingest, ingestionPort, ingestionHostname } }) => {
+        const parsedConfig: PluginsServerConfig = config ? JSON.parse(config) : {}
+        startPluginsServer(parsedConfig)
+        if (ingest)
+            import('./ingestion/server').then(({ startIngestionServer }) =>
+                startIngestionServer(ingestionPort, ingestionHostname)
+            )
     })
     .help().argv
