e5571262f1b053866e4302596448e12bf1d0913f
Marius Andra <marius.andra@gmail.com>
2020-11-26 15:41:22 +0100

    add Argv type and dash-case cli arguments

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,25 @@ import { PluginsServerConfig } from './types'
 import { startPluginsServer } from './server'
 import { startWebServer } from './web/server'
 
+type Argv = {
+    config: string
+    disableWeb: boolean
+    webPort: number
+    webHostname: string
+}
+
 yargs
     .scriptName('posthog-plugins')
     .option('config', { alias: 'c', describe: 'Config options JSON.', type: 'string' })
-    .option('disableWeb', { describe: 'Whether web server should be disabled.', type: 'boolean' })
-    .option('webPort', { alias: 'p', describe: 'Web server port.', type: 'number' })
-    .option('webHostname', { alias: 'h', describe: 'Web server hostname.', type: 'string' })
+    .option('disable-web', { describe: 'Whether web server should be disabled.', type: 'boolean' })
+    .option('web-port', { alias: 'p', describe: 'Web server port.', type: 'number' })
+    .option('web-hostname', { alias: 'h', describe: 'Web server hostname.', type: 'string' })
     .help()
     .command({
         command: ['start', '$0'],
         describe: 'start the server',
-        handler: ({ config, disableWeb, webPort, webHostname }: Record<string, string>) => {
+        handler: ({ config, disableWeb, webPort, webHostname }: Argv) => {
+            console.log({ config, disableWeb, webPort, webHostname })
             const parsedConfig: PluginsServerConfig = config ? JSON.parse(config) : {}
             startPluginsServer(parsedConfig)
             if (!disableWeb) {
