9b3c82d4f767be66a4ec1f23e7ee7e22f8e87955
Marius Andra <marius.andra@gmail.com>
2020-12-05 16:46:24 +0100

    tests for batching performance

--- src/worker/__tests__/piscina.test.ts
+++ src/worker/__tests__/piscina.test.ts
@@ -8,7 +8,7 @@ import * as os from 'os'
 jest.mock('../../sql')
 jest.setTimeout(300000) // 300 sec timeout
 
-function processOneEvent(processEvent: (event: PluginEvent) => Promise<PluginEvent>): Promise<PluginEvent> {
+function processOneEvent(processEvent: (event: PluginEvent) => Promise<PluginEvent>, index: number): Promise<PluginEvent> {
     const defaultEvent = {
         distinct_id: 'my_id',
         ip: '127.0.0.1',
@@ -16,7 +16,7 @@ function processOneEvent(processEvent: (event: PluginEvent) => Promise<PluginEve
         team_id: 2,
         now: new Date().toISOString(),
         event: 'default event',
-        properties: { key: 'value' },
+        properties: { key: 'value', index },
     }
 
     return processEvent(defaultEvent)
@@ -24,29 +24,31 @@ function processOneEvent(processEvent: (event: PluginEvent) => Promise<PluginEve
 
 function processOneBatch(
     processEvents: (events: PluginEvent[]) => Promise<PluginEvent[]>,
-    batchSize: number = 10
+    batchSize: number,
+    batchIndex: number
 ): Promise<PluginEvent[]> {
-    const events = [...Array(batchSize)].map(() => ({
+    const events = [...Array(batchSize)].map((_, i) => ({
         distinct_id: 'my_id',
         ip: '127.0.0.1',
         site_url: 'http://localhost',
         team_id: 2,
         now: new Date().toISOString(),
         event: 'default event',
-        properties: { key: 'value' },
+        properties: { key: 'value', batchIndex, indexInBatch: i },
     }))
 
     return processEvents(events)
 }
 
-async function processCountEvents(count: number, piscina: ReturnType<typeof makePiscina>, batched: false) {
+async function processCountEvents(piscina: ReturnType<typeof makePiscina>, count: number, batchSize = 1) {
     const startTime = performance.now()
     const promises = Array(count)
+
     const processEvent = (event: PluginEvent) => piscina.runTask({ task: 'processEvent', args: { event } })
     const processEvents = (events: PluginEvent[]) => piscina.runTask({ task: 'processEvents', args: { events } })
 
     for (let i = 0; i < count; i++) {
-        promises[i] = processOneEvent(processEvent)
+        promises[i] = batchSize === 1 ? processOneEvent(processEvent, i) : processOneBatch(processEvents, batchSize, i)
     }
     // this will get heavy for tests > 10k events, should chunk them somehow...
     await Promise.all(promises)
@@ -54,7 +56,7 @@ async function processCountEvents(count: number, piscina: ReturnType<typeof make
     const ms = Math.round((performance.now() - startTime) * 1000) / 1000
 
     const log = {
-        eventsPerSecond: 1000 / (ms / count),
+        eventsPerSecond: 1000 / (ms / (count * batchSize)),
         events: count,
         concurrency: piscina.threads.length,
         totalMs: ms,
@@ -65,8 +67,6 @@ async function processCountEvents(count: number, piscina: ReturnType<typeof make
 }
 
 function setupPiscina(workers: number) {
-    console.log(defaultConfig)
-    console.log('!!!!')
     return makePiscina({
         ...defaultConfig,
         WORKER_CONCURRENCY: workers,
@@ -75,6 +75,10 @@ function setupPiscina(workers: number) {
                 let j = 0; for(let i = 0; i < 200000; i++) { j = i };
                 event.properties = { "somewhere": "over the rainbow" }; 
                 return event
+            }
+
+            function processEvents (events, meta) {
+                return events.map(event => processEvent(event, meta))
             }
         `),
     })
@@ -92,12 +96,80 @@ test('piscina 2-24 workers', async () => {
         const piscina = setupPiscina(cores)
 
         // warmup
-        await processCountEvents(cpuCount * 4, piscina)
+        await processCountEvents(piscina, cpuCount * 4, 1)
 
         // start
         let throughput = 0
         for (let i = 0; i < rounds; i++) {
-            const { eventsPerSecond } = await processCountEvents(events, piscina)
+            const { eventsPerSecond } = await processCountEvents(piscina, events, 1)
+            throughput += eventsPerSecond
+        }
+        results[cores] = Math.round(throughput / rounds)
+        await piscina.destroy()
+    }
+
+    console.log({ cpuCount })
+    console.log(JSON.stringify(results, null, 2))
+
+    // expect that adding more cores (up to cpuCount) increases throughput
+    for (let i = 1; i < workers.length; i++) {
+        expect(results[workers[i - 1]]).toBeLessThan(results[workers[i]])
+    }
+})
+
+test('piscina 2-24 workers batch of 10', async () => {
+    const cpuCount = os.cpus().length
+
+    const workers = [1, 2, 4, 8, 12, 16, 24, 32, 48, 64].filter((cores) => cores < cpuCount)
+    const events = 1000
+    const batchSize = 10
+    const rounds = 5
+
+    const results: Record<number, number> = {}
+    for (const cores of workers) {
+        const piscina = setupPiscina(cores)
+
+        // warmup
+        await processCountEvents(piscina, cpuCount * 4, batchSize)
+
+        // start
+        let throughput = 0
+        for (let i = 0; i < rounds; i++) {
+            const { eventsPerSecond } = await processCountEvents(piscina, events, batchSize)
+            throughput += eventsPerSecond
+        }
+        results[cores] = Math.round(throughput / rounds)
+        await piscina.destroy()
+    }
+
+    console.log({ cpuCount })
+    console.log(JSON.stringify(results, null, 2))
+
+    // expect that adding more cores (up to cpuCount) increases throughput
+    for (let i = 1; i < workers.length; i++) {
+        expect(results[workers[i - 1]]).toBeLessThan(results[workers[i]])
+    }
+})
+
+test('piscina 2-24 workers batch of 100', async () => {
+    const cpuCount = os.cpus().length
+
+    const workers = [1, 2, 4, 8, 12, 16, 24, 32, 48, 64].filter((cores) => cores < cpuCount)
+    const events = 100
+    const batchSize = 100
+    const rounds = 5
+
+    const results: Record<number, number> = {}
+    for (const cores of workers) {
+        const piscina = setupPiscina(cores)
+
+        // warmup
+        await processCountEvents(piscina, cpuCount * 4, batchSize)
+
+        // start
+        let throughput = 0
+        for (let i = 0; i < rounds; i++) {
+            const { eventsPerSecond } = await processCountEvents(piscina, events, batchSize)
             throughput += eventsPerSecond
         }
         results[cores] = Math.round(throughput / rounds)
