73972826206c28411553ba6f792d6d3dc7721d60
Marius Andra <marius.andra@gmail.com>
2020-12-09 12:00:17 +0100

    add some comments

--- src/init.ts
+++ src/init.ts
@@ -1,8 +1,11 @@
 import { PluginsServerConfig } from './types'
 import { setLogLevel } from './utils'
 import * as Sentry from '@sentry/node'
-import * as Tracing from '@sentry/tracing' // unused import, yet importing inits globals
 
+// Must require as `tsc` strips unused `import` statements and just requiring this seems to init some globals
+require('@sentry/tracing')
+
+// Code that runs on app start, in both the main and worker threads
 export function initApp(config: PluginsServerConfig) {
     setLogLevel(config.LOG_LEVEL)
 
@@ -11,6 +14,7 @@ export function initApp(config: PluginsServerConfig) {
             dsn: config.SENTRY_DSN,
             // We recommend adjusting this value in production, or using tracesSampler for finer control
             tracesSampleRate: 1.0,
+            debug: true,
         })
     }
 }
