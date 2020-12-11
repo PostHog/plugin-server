c4c6c79574e873e141fc1d7915902c2e355dede9
Marius Andra <marius.andra@gmail.com>
2020-12-11 23:51:22 +0100

    refactor

--- src/extensions/posthog.ts
+++ src/extensions/posthog.ts
@@ -6,29 +6,33 @@ export function createPostHog(server: PluginsServer, pluginConfig: PluginConfig)
     const state = {
         distinctId: `${pluginConfig.plugin?.name} (${pluginConfig.id})`,
     }
+
+    function sendEvent(event: string, properties: Record<string, any> = {}) {
+        const client = new Client(server.redis, server.PLUGINS_CELERY_QUEUE)
+
+        const data = {
+            distinct_id: state.distinctId,
+            event,
+            timestamp: new Date(),
+            properties: {
+                $lib: 'posthog-plugin-server',
+                $lib_version: version,
+                ...properties,
+            },
+        }
+
+        client.sendTask(
+            'posthog.tasks.process_event.process_event_with_plugins',
+            [state.distinctId, null, null, data, pluginConfig.team_id, new Date(), new Date()],
+            {}
+        )
+    }
     return {
         init(distinctId: string) {
             state.distinctId = distinctId
         },
         capture(event: string, properties: Record<string, any> = {}) {
-            const client = new Client(server.redis, server.PLUGINS_CELERY_QUEUE)
-
-            const data = {
-                distinct_id: state.distinctId,
-                event,
-                timestamp: new Date(),
-                properties: {
-                    $lib: 'posthog-plugin-server',
-                    $lib_version: version,
-                    ...properties,
-                },
-            }
-
-            client.sendTask(
-                'posthog.tasks.process_event.process_event_with_plugins',
-                [state.distinctId, null, null, data, pluginConfig.team_id, new Date(), new Date()],
-                {}
-            )
+            sendEvent(event, properties)
         },
     }
 }
