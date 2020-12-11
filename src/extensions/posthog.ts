98d98068f5d8acf4bee7934eeb8701bdaa9702b4
Marius Andra <marius.andra@gmail.com>
2020-12-11 22:44:48 +0100

    posthog instance that sends directly to celery

---
+++ src/extensions/posthog.ts
@@ -0,0 +1,38 @@
+import { PluginsServer, PluginConfig } from 'types'
+import { version } from '../../package.json'
+import Client from '../celery/client'
+
+export function createPostHog(server: PluginsServer, pluginConfig: PluginConfig) {
+    const state = {
+        distinctId: `${pluginConfig.plugin?.name} (${pluginConfig.id})`,
+    }
+    return {
+        init(distinctId: string) {
+            state.distinctId = distinctId
+        },
+        capture(event: string, properties: Record<string, any> = {}) {
+            const client = new Client(server.redis, server.PLUGINS_CELERY_QUEUE)
+
+            const data = {
+                distinct_id: state.distinctId,
+                event,
+                timestamp: new Date(),
+                properties: {
+                    $lib: 'posthog-plugin-server',
+                    $lib_version: version,
+                    ...properties,
+                },
+            }
+
+            client.sendTask('posthog.tasks.process_event.process_event', [], {
+                distinct_id: state.distinctId,
+                ip: null,
+                site_url: null,
+                data,
+                team_id: pluginConfig.team_id,
+                now: new Date(),
+                sent_at: new Date(),
+            })
+        },
+    }
+}
