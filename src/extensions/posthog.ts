98bc7271cefd42a1f8aca8424373b7298ead185c
Michael Matloka <dev@twixes.com>
2020-12-17 16:39:45 +0100

    Make code more elegant

--- src/extensions/posthog.ts
+++ src/extensions/posthog.ts
@@ -12,67 +12,65 @@ export interface DummyPostHog {
 export function createPosthog(server: PluginsServer, pluginConfig: PluginConfig): DummyPostHog {
     const distinctId = pluginConfig.plugin?.name || `plugin-id-${pluginConfig.plugin_id}`
 
-    const producer = server.EE_ENABLED ? server.kafka!.producer() : null
+    const client = server.EE_ENABLED ? null : new Client(server.redis, server.PLUGINS_CELERY_QUEUE) // Redis
+    const producer = server.EE_ENABLED ? server.kafka!.producer() : null // Kafka
     producer?.connect()
 
-    const sendEvent = server.EE_ENABLED
-        ? (event: string, properties: Record<string, any> = {}) => {
-              const uuid = new UUIDT().toString()
-              const utcNow = DateTime.utc().toISO()
-              const data = {
-                  distinct_id: distinctId,
-                  event,
-                  timestamp: utcNow,
-                  properties: {
-                      $lib: 'posthog-plugin-server',
-                      $lib_version: version,
-                      ...properties,
-                  },
-              }
+    function sendEventRedis(event: string, properties: Record<string, any> = {}) {
+        const utcNow = DateTime.utc().toISO()
+        const data = {
+            distinct_id: distinctId,
+            event,
+            timestamp: utcNow,
+            properties: {
+                $lib: 'posthog-plugin-server',
+                $lib_version: version,
+                ...properties,
+            },
+        }
 
-              producer!.send({
-                  topic: KAFKA_EVENTS_WAL,
-                  messages: [
-                      {
-                          key: uuid,
-                          value: JSON.stringify({
-                              distinct_id: distinctId,
-                              ip: '',
-                              site_url: '',
-                              data: JSON.stringify(data),
-                              team_id: pluginConfig.team_id,
-                              now: utcNow,
-                              sent_at: utcNow,
-                              uuid,
-                          } as RawEventMessage),
-                      },
-                  ],
-              })
-          }
-        : (event: string, properties: Record<string, any> = {}) => {
-              const utcNow = DateTime.utc().toISO()
-              const client = new Client(server.redis, server.PLUGINS_CELERY_QUEUE)
+        client!.sendTask(
+            'posthog.tasks.process_event.process_event_with_plugins',
+            [distinctId, null, null, data, pluginConfig.team_id, utcNow, utcNow],
+            {}
+        )
+    }
 
-              const data = {
-                  distinct_id: distinctId,
-                  event,
-                  timestamp: utcNow,
-                  properties: {
-                      $lib: 'posthog-plugin-server',
-                      $lib_version: version,
-                      ...properties,
-                  },
-              }
+    function sendEventKafka(event: string, properties: Record<string, any> = {}) {
+        const utcNow = DateTime.utc().toISO()
+        const uuid = new UUIDT().toString()
+        const data = {
+            distinct_id: distinctId,
+            event,
+            timestamp: utcNow,
+            properties: {
+                $lib: 'posthog-plugin-server',
+                $lib_version: version,
+                ...properties,
+            },
+        }
 
-              if (server.EE_ENABLED) {
-              } else {
-                  client.sendTask(
-                      'posthog.tasks.process_event.process_event_with_plugins',
-                      [distinctId, null, null, data, pluginConfig.team_id, utcNow, utcNow],
-                      {}
-                  )
-              }
-          }
+        producer!.send({
+            topic: KAFKA_EVENTS_WAL,
+            messages: [
+                {
+                    key: uuid,
+                    value: JSON.stringify({
+                        distinct_id: distinctId,
+                        ip: '',
+                        site_url: '',
+                        data: JSON.stringify(data),
+                        team_id: pluginConfig.team_id,
+                        now: utcNow,
+                        sent_at: utcNow,
+                        uuid,
+                    } as RawEventMessage),
+                },
+            ],
+        })
+    }
+
+    const sendEvent = server.EE_ENABLED ? sendEventKafka : sendEventRedis
 
     return {
         capture(event, properties = {}) {
