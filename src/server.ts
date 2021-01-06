7bed47e999eaaeeb381d9b098db86b2675b025ca
Marius Andra <marius.andra@gmail.com>
2021-01-06 23:06:49 +0100

    fix lazy-ass merge

--- src/server.ts
+++ src/server.ts
@@ -35,7 +35,7 @@ export async function createServer(
         })
         .on('ready', () => {
             if (process.env.NODE_ENV !== 'test') {
-                status.info(`✅ Connected to Redis!`)
+                status.info('✅', 'Connected to Redis!')
             }
         })
     await redis.info()
