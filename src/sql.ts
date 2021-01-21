c8a87ea5a99ecb6d2b05de5539fa1505be15866e
Marius Andra <marius.andra@gmail.com>
2021-01-21 11:31:21 +0100

    do not get columns from JOINed tables (results in conflicting names like `id`)

--- src/sql.ts
+++ src/sql.ts
@@ -2,7 +2,7 @@ import { Plugin, PluginAttachmentDB, PluginConfig, PluginConfigId, PluginError, 
 
 export async function getPluginRows(server: PluginsServer): Promise<Plugin[]> {
     const { rows: pluginRows }: { rows: Plugin[] } = await server.db.query(
-        `SELECT * FROM posthog_plugin WHERE id in
+        `SELECT posthog_plugin.* FROM posthog_plugin WHERE id in
             (SELECT posthog_pluginconfig.plugin_id
                 FROM posthog_pluginconfig
                 LEFT JOIN posthog_team ON posthog_team.id = posthog_pluginconfig.team_id
@@ -14,7 +14,7 @@ export async function getPluginRows(server: PluginsServer): Promise<Plugin[]> {
 
 export async function getPluginAttachmentRows(server: PluginsServer): Promise<PluginAttachmentDB[]> {
     const { rows }: { rows: PluginAttachmentDB[] } = await server.db.query(
-        `SELECT * FROM posthog_pluginattachment WHERE plugin_config_id in
+        `SELECT posthog_pluginattachment.* FROM posthog_pluginattachment WHERE plugin_config_id in
             (SELECT posthog_pluginconfig.id
                 FROM posthog_pluginconfig
                 LEFT JOIN posthog_team ON posthog_team.id = posthog_pluginconfig.team_id
@@ -25,7 +25,7 @@ export async function getPluginAttachmentRows(server: PluginsServer): Promise<Pl
 
 export async function getPluginConfigRows(server: PluginsServer): Promise<PluginConfig[]> {
     const { rows }: { rows: PluginConfig[] } = await server.db.query(
-        `SELECT *
+        `SELECT posthog_pluginconfig.*
             FROM posthog_pluginconfig
             LEFT JOIN posthog_team ON posthog_team.id = posthog_pluginconfig.team_id
             WHERE (posthog_team.id IS NULL OR posthog_team.plugins_opt_in='t') AND posthog_pluginconfig.enabled='t'`
