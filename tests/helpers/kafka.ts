438fa3bb98ebf738aa7398b4759642be126c4bb7
Michael Matloka <dev@twixes.com>
2021-01-27 12:57:12 +0100

    Debug KafkaObserver

--- tests/helpers/kafka.ts
+++ tests/helpers/kafka.ts
@@ -28,18 +28,24 @@ export class KafkaObserver extends EventEmitter {
     }
 
     public async start(): Promise<void> {
+        console.info('observer started!')
         if (this.isStarted) {
             return
         }
         this.isStarted = true
         return await new Promise<void>(async (resolve, reject) => {
+            console.info('connecting producer')
             await this.producer.connect()
+            console.info('subscribing consumer')
             await this.consumer.subscribe({ topic: KAFKA_EVENTS })
+            console.info('running consumer')
             await this.consumer.run({
                 eachMessage: async (payload) => {
+                    console.info('message received!')
                     this.emit('message', payload)
                 },
             })
+            console.info('setting group join and crash listeners')
             const { GROUP_JOIN, CRASH } = this.consumer.events
             this.consumer.on(GROUP_JOIN, () => resolve())
             this.consumer.on(CRASH, ({ payload: { error } }) => reject(error))
@@ -48,13 +54,17 @@ export class KafkaObserver extends EventEmitter {
 
     public async stop(): Promise<void> {
         this.removeAllListeners()
-        await this.consumer.stop()
-        await this.consumer.disconnect()
+        console.info('disconnecting producer')
         await this.producer.disconnect()
+        console.info('stopping consumer')
+        await this.consumer.stop()
+        console.info('disconnecting consumer')
+        await this.consumer.disconnect()
     }
 
     public async handOffMessage(message: EventMessage): Promise<void> {
-        this.producer.send({
+        console.info('producing message')
+        await this.producer.send({
             topic: KAFKA_EVENTS_INGESTION_HANDOFF,
             messages: [{ value: Buffer.from(JSON.stringify(message)) }],
         })
@@ -62,17 +72,20 @@ export class KafkaObserver extends EventEmitter {
 
     public async waitForProcessedMessages(numberOfMessages: number): Promise<EventMessage[]> {
         return await new Promise<EventMessage[]>((resolve, reject) => {
+            console.info('waiting for processed messages')
             const accumulator: EventMessage[] = []
             const timeoutSeconds = numberOfMessages * 2 // give every message 2 s on average to show up
-            setTimeout(
+            const rejectTimeout = setTimeout(
                 () =>
                     reject(`Timed out waiting ${timeoutSeconds} seconds for ${numberOfMessages} message(s) from Kafka`),
                 timeoutSeconds * 1000
             )
             const onMessage = (payload: EachMessagePayload) => {
+                console.info('message received')
                 accumulator.push(parseRawEventMessage(JSON.parse(payload.message.value!.toString())))
                 if (accumulator.length >= numberOfMessages) {
                     this.removeListener('message', onMessage)
+                    clearTimeout(rejectTimeout)
                     resolve(accumulator)
                 }
             }
