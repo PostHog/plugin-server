e8d3336c5923626a78e03d6f5b8adec21ac48cec
Marius Andra <marius.andra@gmail.com>
2021-02-02 20:01:13 +0100

    reuse kafkaProducer on server

--- tests/clickhouse/e2e.test.ts
+++ tests/clickhouse/e2e.test.ts
@@ -46,8 +46,6 @@ describe('e2e clickhouse ingestion', () => {
 
     afterEach(async () => {
         await stopServer()
-        // TODO: this needs a better way to stop
-        ;(createPosthog as any).producer.disconnect()
     })
 
     test('event captured, processed, ingested', async () => {
