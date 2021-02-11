5588babaa1ba81e53a4a7e914798209d92c49947
Marius Andra <marius.andra@gmail.com>
2021-02-11 10:30:13 +0100

    ingest in piscina worker

--- src/worker/queue.ts
+++ src/worker/queue.ts
@@ -1,37 +1,61 @@
 import { PluginEvent } from '@posthog/plugin-scaffold'
 import * as Sentry from '@sentry/node'
-import { DateTime } from 'luxon'
+import Piscina from 'piscina'
 
 import Client from '../celery/client'
 import Worker from '../celery/worker'
+import { IngestEventResponse } from '../ingestion/ingest-event'
 import { KafkaQueue } from '../ingestion/kafka-queue'
 import { status } from '../status'
 import { PluginsServer, Queue } from '../types'
-import { UUIDT } from '../utils'
+
+export type WorkerMethods = {
+    processEvent: (queue: Queue, event: PluginEvent) => Promise<PluginEvent | null>
+    processEventBatch: (queue: Queue, event: PluginEvent[]) => Promise<(PluginEvent | null)[]>
+    ingestEvent: (queue: Queue, event: PluginEvent) => Promise<IngestEventResponse>
+}
+
+function pauseQueueIfFull(queue: Queue, server: PluginsServer, piscina?: Piscina) {
+    if ((piscina?.queueSize || 0) > (server.WORKER_CONCURRENCY || 4) * (server.WORKER_CONCURRENCY || 4)) {
+        queue.pause()
+    }
+}
 
 export async function startQueue(
     server: PluginsServer,
-    processEvent: (event: PluginEvent) => Promise<PluginEvent | null>,
-    processEventBatch: (event: PluginEvent[]) => Promise<(PluginEvent | null)[]>
+    piscina?: Piscina,
+    workerMethods: Partial<WorkerMethods> = {}
 ): Promise<Queue> {
     const relevantStartQueue = server.KAFKA_ENABLED ? startQueueKafka : startQueueRedis
+    const mergedWorkerMethods = {
+        processEvent: (queue: Queue, event: PluginEvent) => {
+            pauseQueueIfFull(queue, server, piscina)
+            return piscina!.runTask({ task: 'processEvent', args: { event } })
+        },
+        processEventBatch: (queue: Queue, batch: PluginEvent[]) => {
+            pauseQueueIfFull(queue, server, piscina)
+            return piscina!.runTask({ task: 'processEventBatch', args: { batch } })
+        },
+        ingestEvent: (queue: Queue, event: PluginEvent) => {
+            pauseQueueIfFull(queue, server, piscina)
+            return piscina!.runTask({ task: 'ingestEvent', args: { event } })
+        },
+        ...workerMethods,
+    }
+
     try {
-        return await relevantStartQueue(server, processEvent, processEventBatch)
+        return await relevantStartQueue(server, mergedWorkerMethods)
     } catch (error) {
         status.error('💥', 'Failed to start event queue:\n', error)
         throw error
     }
 }
 
-async function startQueueRedis(
-    server: PluginsServer,
-    processEvent: (event: PluginEvent) => Promise<PluginEvent | null>,
-    processEventBatch: (event: PluginEvent[]) => Promise<(PluginEvent | null)[]>
-): Promise<Queue> {
-    const worker = new Worker(server.redis, server.PLUGINS_CELERY_QUEUE)
+async function startQueueRedis(server: PluginsServer, workerMethods: WorkerMethods): Promise<Queue> {
+    const celeryQueue = new Worker(server.redis, server.PLUGINS_CELERY_QUEUE)
     const client = new Client(server.redis, server.CELERY_DEFAULT_QUEUE)
 
-    worker.register(
+    celeryQueue.register(
         'posthog.tasks.process_event.process_event_with_plugins',
         async (
             distinct_id: string,
@@ -44,22 +68,12 @@ async function startQueueRedis(
         ) => {
             const event = { distinct_id, ip, site_url, team_id, now, sent_at, ...data } as PluginEvent
             try {
-                const processedEvent = await processEvent(event)
+                const processedEvent = await workerMethods.processEvent(celeryQueue, event)
                 if (processedEvent) {
-                    const { distinct_id, ip, site_url, team_id, now, sent_at, ...data } = processedEvent
-
                     if (server.PLUGIN_SERVER_INGESTION) {
-                        await server.eventsProcessor.processEvent(
-                            distinct_id,
-                            ip,
-                            site_url,
-                            processedEvent,
-                            team_id,
-                            DateTime.fromISO(now),
-                            sent_at ? DateTime.fromISO(sent_at) : null,
-                            new UUIDT().toString()
-                        )
+                        await workerMethods.ingestEvent(celeryQueue, processedEvent)
                     } else {
+                        const { distinct_id, ip, site_url, team_id, now, sent_at, ...data } = processedEvent
                         client.sendTask('posthog.tasks.process_event.process_event', [], {
                             distinct_id,
                             ip,
@@ -77,36 +91,23 @@ async function startQueueRedis(
         }
     )
 
-    worker.start()
+    celeryQueue.start()
 
-    return worker
+    return celeryQueue
 }
 
-async function startQueueKafka(
-    server: PluginsServer,
-    processEvent: (event: PluginEvent) => Promise<PluginEvent | null>,
-    processEventBatch: (event: PluginEvent[]) => Promise<(PluginEvent | null)[]>
-): Promise<Queue> {
-    const kafkaQueue = new KafkaQueue(server, processEventBatch, async (event: PluginEvent) => {
-        const { distinct_id, ip, site_url, team_id, now, sent_at, uuid } = event
-        if (!uuid) {
-            status.error('❓', 'UUID missing in event received from Kafka!')
-            return
-        }
-        if (server.PLUGIN_SERVER_INGESTION) {
-            await server.eventsProcessor.processEvent(
-                distinct_id,
-                ip,
-                site_url,
-                event,
-                team_id,
-                DateTime.fromISO(now),
-                sent_at ? DateTime.fromISO(sent_at) : null,
-                uuid
-            )
-        }
-    })
-
+async function startQueueKafka(server: PluginsServer, workerMethods: WorkerMethods): Promise<Queue> {
+    const kafkaQueue: Queue = new KafkaQueue(
+        server,
+        (batch: PluginEvent[]) => workerMethods.processEventBatch(kafkaQueue, batch),
+        server.PLUGIN_SERVER_INGESTION
+            ? async (event) => {
+                  await workerMethods.ingestEvent(kafkaQueue, event)
+              }
+            : async () => {
+                  // no op, but defining to avoid undefined issues
+              }
+    )
     await kafkaQueue.start()
 
     return kafkaQueue
