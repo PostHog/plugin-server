aa016aa2eb0c6f28b2dc38fae4a2d42950bd0a38
Marius Andra <marius.andra@gmail.com>
2021-02-16 17:16:11 +0100

    add types to loop timeout

--- src/vm/transforms/loop-timeout.ts
+++ src/vm/transforms/loop-timeout.ts
@@ -2,14 +2,28 @@
 // https://medium.com/@bvjebin/js-infinite-loops-killing-em-e1c2f5f2db7f
 // https://github.com/jsbin/loop-protect/blob/master/lib/index.js
 
+import * as types from '@babel/types'
+
 import { PluginsServer } from '../../types'
 
-const generateBefore = (t: any, id: any) =>
+const generateBefore = (t: typeof types, id: any) =>
     t.variableDeclaration('var', [
         t.variableDeclarator(id, t.callExpression(t.memberExpression(t.identifier('Date'), t.identifier('now')), [])),
     ])
 
-const generateInside = ({ t, id, line, ch, timeout }: any = {}) => {
+const generateInside = ({
+    t,
+    id,
+    line,
+    ch,
+    timeout,
+}: {
+    t: typeof types
+    id: types.Identifier
+    line: number
+    ch: number
+    timeout: number
+}) => {
     return t.ifStatement(
         t.binaryExpression(
             '>',
@@ -21,7 +35,7 @@ const generateInside = ({ t, id, line, ch, timeout }: any = {}) => {
             t.numericLiteral(timeout * 1000)
         ),
         t.throwStatement(
-            t.NewExpression(t.identifier('Error'), [
+            t.newExpression(t.identifier('Error'), [
                 t.stringLiteral(
                     `Script execution timed out after looping for ${timeout} second${
                         timeout === 1 ? '' : 's'
@@ -32,7 +46,7 @@ const generateInside = ({ t, id, line, ch, timeout }: any = {}) => {
     )
 }
 
-const protect = (t: any, timeout: number) => (path: any) => {
+const protect = (t: typeof types, timeout: number) => (path: any): void => {
     if (!path.node.loc) {
         // I don't really know _how_ we get into this state
         // but https://jsbin.com/mipesawapi/1/ triggers it
@@ -59,8 +73,7 @@ const protect = (t: any, timeout: number) => (path: any) => {
     body.unshiftContainer('body', inside)
 }
 
-export const loopTimeout = (server: PluginsServer) => (babel: any) => {
-    const t = babel.types
+export const loopTimeout = (server: PluginsServer) => ({ types: t }: { types: typeof types }) => {
     return {
         visitor: {
             WhileStatement: protect(t, server.TASK_TIMEOUT),
