f2aeff86f684768e406e4cff53bbd7f7271ea46b
Michael Matloka <dev@twixes.com>
2021-02-17 21:39:56 +0100

    Improve timeoutGuard default timeout

--- src/ingestion/utils.ts
+++ src/ingestion/utils.ts
@@ -1,6 +1,7 @@
 import * as Sentry from '@sentry/node'
 import crypto from 'crypto'
 
+import { defaultConfig } from '../config'
 import { BasePerson, Element, Person, RawPerson } from '../types'
 
 export function unparsePersonPartial(person: Partial<Person>): Partial<RawPerson> {
@@ -156,7 +157,7 @@ export function chainToElements(chain: string): Element[] {
     return elements
 }
 
-export function timeoutGuard(message: string, timeout = 30000): NodeJS.Timeout {
+export function timeoutGuard(message: string, timeout = defaultConfig.TASK_TIMEOUT * 1000): NodeJS.Timeout {
     return setTimeout(() => {
         console.log(`⌛⌛⌛ ${message}`)
         Sentry.captureMessage(message)
