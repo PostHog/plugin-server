c35bf868869f3ec0cc462290e2e477b8b57952b1
Michael Matloka <dev@twixes.com>
2021-03-01 13:34:26 +0100

    Fix healthStatus usage

--- src/healthcheck.ts
+++ src/healthcheck.ts
@@ -8,10 +8,10 @@ const healthStatus = new Status('HLTH')
 const redis = new Redis(defaultConfig.REDIS_URL).on('ready', async () => {
     const ping = await redis.get('@posthog-plugin-server/ping')
     if (ping) {
-        healthStatus.error('💔', `Redis key @posthog-plugin-server/ping found with value ${ping}`)
+        healthStatus.info('💚', `Redis key @posthog-plugin-server/ping found with value ${ping}`)
         process.exit(0)
     } else {
-        healthStatus.error('💚', 'Redis key @posthog-plugin-server/ping not found! Plugin server seems to be offline')
+        healthStatus.error('💔', 'Redis key @posthog-plugin-server/ping not found! Plugin server seems to be offline')
         process.exit(1)
     }
 })
