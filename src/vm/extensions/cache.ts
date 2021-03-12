44eba0030a162db25b732c88d4d37b15bb732385
Michael Matloka <dev@twixes.com>
2021-03-12 16:13:14 +0100

    Allow JSON parsing/stringification option in cache extension

--- src/vm/extensions/cache.ts
+++ src/vm/extensions/cache.ts
@@ -5,11 +5,11 @@ import { PluginsServer } from '../../types'
 export function createCache(server: PluginsServer, pluginId: number, teamId: number): CacheExtension {
     const getKey = (key: string) => `@plugin/${pluginId}/${typeof teamId === 'undefined' ? '@all' : teamId}/${key}`
     return {
-        set: async function (key: string, value: unknown, ttlSeconds?: number): Promise<void> {
-            return await server.db.redisSet(getKey(key), value, ttlSeconds)
+        set: async function (key: string, value: unknown, ttlSeconds?: number, jsonStringify = true): Promise<void> {
+            return await server.db.redisSet(getKey(key), value, ttlSeconds, jsonStringify)
         },
-        get: async function (key: string, defaultValue: unknown): Promise<unknown> {
-            return await server.db.redisGet(getKey(key), defaultValue)
+        get: async function (key: string, defaultValue: unknown, jsonParse = true): Promise<unknown> {
+            return await server.db.redisGet(getKey(key), defaultValue, jsonParse)
         },
         incr: async function (key: string): Promise<number> {
             return await server.db.redisIncr(getKey(key))
