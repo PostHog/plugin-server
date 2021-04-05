865dd53324d4331c10217ce2dd5ed4804b3cf3f4
Michael Matloka <dev@twixes.com>
2021-04-05 19:28:12 +0200

    Remove yargs

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,7 @@
-import * as yargs from 'yargs'
-
 import { initApp } from './init'
 import { startPluginsServer } from './main/pluginsServer'
-import { configHelp, defaultConfig } from './shared/config'
-import { PluginsServerConfig } from './types'
+import { defaultConfig } from './shared/config'
 import { makePiscina } from './worker/piscina'
 
-type Argv = {
-    config: string
-    disableWeb: boolean
-    webPort: number
-    webHostname: string
-    concurrency: number
-}
-
-let app: any = yargs
-    .wrap(yargs.terminalWidth())
-    .scriptName('posthog-plugins')
-    .option('config', { alias: 'c', describe: 'Config options JSON.', type: 'string' })
-
-for (const [key, value] of Object.entries(defaultConfig)) {
-    app = app.option(key.toLowerCase().split('_').join('-'), {
-        describe: `${configHelp[key] || key} [${value}]`,
-        type: typeof value,
-    })
-}
-
-const { config: configArg, ...otherArgs }: Argv = app.help().argv
-
-const configJson = configArg || process.env.CONFIG
-const config: PluginsServerConfig = { ...defaultConfig, ...(configJson ? JSON.parse(configJson) : {}) }
-
-for (const [key, value] of Object.entries(otherArgs)) {
-    if (typeof value !== 'undefined') {
-        // convert camelCase argument keys to under_score
-        const newKey = key
-            .replace(/(?:^|\.?)([A-Z])/g, (x, y) => '_' + y.toUpperCase())
-            .replace(/^_/, '')
-            .toUpperCase()
-        if (newKey in defaultConfig) {
-            config[newKey] = value
-        }
-    }
-}
-
-initApp(config)
-void startPluginsServer(config, makePiscina) // void the returned promise
+initApp(defaultConfig)
+void startPluginsServer(defaultConfig, makePiscina) // void the returned promise
