1fe9bc569cc39a0688d427580c676e14a1b3515f
Marius Andra <marius.andra@gmail.com>
2021-04-09 10:01:28 +0200

    revert server test

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -1,8 +1,7 @@
 import { startPluginsServer } from '../src/main/pluginsServer'
 import { LogLevel } from '../src/types'
 import { makePiscina } from '../src/worker/piscina'
-import { pluginConfig39 } from './helpers/plugins'
-import { getErrorForPluginConfig, resetTestDatabase } from './helpers/sql'
+import { resetTestDatabase } from './helpers/sql'
 
 jest.mock('../src/shared/sql')
 jest.setTimeout(60000) // 60 sec timeout
@@ -11,9 +10,6 @@ test('startPluginsServer', async () => {
     const testCode = `
         async function processEvent (event) {
             return event
-        }
-        async function teardownPlugin() {
-            throw new Error('This Happened In The Teardown Palace')
         }
     `
     await resetTestDatabase(testCode)
@@ -25,13 +21,5 @@ test('startPluginsServer', async () => {
         makePiscina
     )
 
-    // no error before stopping
-    const error1 = await getErrorForPluginConfig(pluginConfig39.id)
-    expect(error1).toBe(null)
-
     await pluginsServer.stop()
-
-    // verify the teardownPlugin code runs
-    const error2 = await getErrorForPluginConfig(pluginConfig39.id)
-    expect(error2.message).toBe('This Happened In The Teardown Palace')
 })
