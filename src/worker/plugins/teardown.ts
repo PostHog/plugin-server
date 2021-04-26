397fc31bd6f65c5d7af697c2ffe214020a9673b5
Michael Matloka <dev@twixes.com>
2021-04-26 15:26:59 +0200

    Update teardown.ts

--- src/worker/plugins/teardown.ts
+++ src/worker/plugins/teardown.ts
@@ -14,29 +14,27 @@ export async function teardownPlugins(server: PluginsServer, pluginConfig?: Plug
                         try {
                             await teardownPlugin()
 
-                            if (!server.ENABLE_PERSISTENT_CONSOLE) {
-                                return
+                            if (server.ENABLE_PERSISTENT_CONSOLE) {
+                                await server.db.createPluginLogEntry(
+                                    pluginConfig,
+                                    PluginLogEntryType.Info,
+                                    true,
+                                    `Plugin unloaded (instance ID ${server.instanceId}).`,
+                                    server.instanceId
+                                )
                             }
-                            await server.db.createPluginLogEntry(
-                                pluginConfig,
-                                PluginLogEntryType.Info,
-                                true,
-                                `Plugin unloaded (instance ID ${server.instanceId}).`,
-                                server.instanceId
-                            )
                         } catch (error) {
                             await processError(server, pluginConfig, error)
 
-                            if (!server.ENABLE_PERSISTENT_CONSOLE) {
-                                return
+                            if (server.ENABLE_PERSISTENT_CONSOLE) {
+                                await server.db.createPluginLogEntry(
+                                    pluginConfig,
+                                    PluginLogEntryType.Error,
+                                    true,
+                                    `Plugin failed to unload (instance ID ${server.instanceId}).`,
+                                    server.instanceId
+                                )
                             }
-                            await server.db.createPluginLogEntry(
-                                pluginConfig,
-                                PluginLogEntryType.Error,
-                                true,
-                                `Plugin failed to unload (instance ID ${server.instanceId}).`,
-                                server.instanceId
-                            )
                         }
                     })()
                 )
