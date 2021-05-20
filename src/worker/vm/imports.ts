de1d56a6cfebb1160ab6e09740415386c3141d25
yakkomajuri <yakko.majuri@gmail.com>
2021-05-20 10:17:15 -0300

    address review

--- src/worker/vm/imports.ts
+++ src/worker/vm/imports.ts
@@ -5,53 +5,52 @@ import crypto from 'crypto'
 import dns from 'dns'
 import * as genericPool from 'generic-pool'
 import ipRangeCheck from 'ip-range-check'
-import nodeFetch from 'node-fetch'
+import net from 'net'
+import nodeFetch, { RequestInit } from 'node-fetch'
 import nodePostgres from 'pg'
 import { parse } from 'pg-connection-string'
 import snowflake from 'snowflake-sdk'
 import url from 'url'
 import * as zlib from 'zlib'
 
+import { IllegalOperationError } from './../plugins/run'
 import { writeToFile } from './extensions/test-utils'
+
+interface URLLike {
+    href: string
+}
 
 // 93.184.216.34 = example.com
 const RESTRICTED_IP_RANGES = ['93.184.216.34', '172.31.0.0/16']
 
-const isIpv6 = (str: string) =>
-    /(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))/.test(
-        str
-    )
-
 const validateHostOrUrl = async (hostOrUrl: any) => {
-    if (typeof hostOrUrl != 'string') {
-        throw new Error(`Invalid host/URL ${hostOrUrl}`)
+    if (typeof hostOrUrl !== 'string') {
+        throw new IllegalOperationError(`Invalid host/URL ${hostOrUrl}: Not a string or URLLike object`)
     }
-    if (hostOrUrl.includes('localhost') || isIpv6(hostOrUrl)) {
-        throw new Error(`Host ${hostOrUrl} is not allowed`)
+    if (hostOrUrl.includes('localhost') || net.isIP(hostOrUrl)) {
+        throw new IllegalOperationError(`${hostOrUrl} is not allowed for security reasons`)
     }
 
     const parsedHost = url.parse(hostOrUrl).hostname
-
-    if (
-        !parsedHost || // IPv4 addresses return a null host, IPv6 addresses do not
-        (parsedHost.includes('amazonaws.com') && parsedHost !== 'redshift.amazonaws.com') ||
-        parsedHost.includes('posthog.net')
-    ) {
-        throw new Error(`Invalid hostname for ${hostOrUrl}`)
+    if (!parsedHost) {
+        throw new IllegalOperationError(`Could not find a hostname for ${hostOrUrl}`)
     }
 
     const lookupResult = await dns.promises.lookup(parsedHost)
 
     for (const range of RESTRICTED_IP_RANGES) {
         if (ipRangeCheck(lookupResult.address, range)) {
-            throw new Error(`Host ${parsedHost} is not allowed`)
+            throw new IllegalOperationError(`Host ${parsedHost} is not allowed for security reasons`)
         }
     }
 }
 
-const fetch = async (url: string, ...args: any) => {
+const fetch = async (url: string | URLLike, init?: RequestInit) => {
+    if (typeof url === 'object' && 'href' in url) {
+        url = url.href
+    }
     await validateHostOrUrl(url)
-    return await nodeFetch(url, ...args)
+    return await nodeFetch(url, init)
 }
 
 class Client extends nodePostgres.Client {
@@ -63,7 +62,7 @@ class Client extends nodePostgres.Client {
             database = config.database || ''
         }
         if (database === 'posthog') {
-            throw new Error('Database name posthog not allowed')
+            throw new IllegalOperationError('Database name posthog not allowed')
         }
         super(config)
     }
