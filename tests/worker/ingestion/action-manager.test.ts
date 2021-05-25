d768f1148b6230c849d71f328f81623bcee0d01f
Michael Matloka <dev@twixes.com>
2021-05-25 16:12:08 +0200

    It's a hub now

--- tests/worker/ingestion/action-manager.test.ts
+++ tests/worker/ingestion/action-manager.test.ts
@@ -1,17 +1,17 @@
-import { PluginsServer, PropertyOperator } from '../../../src/types'
-import { createServer } from '../../../src/utils/db/server'
+import { Hub, PropertyOperator } from '../../../src/types'
+import { createHub } from '../../../src/utils/db/hub'
 import { ActionManager } from '../../../src/worker/ingestion/action-manager'
 import { resetTestDatabase } from '../../helpers/sql'
 
 describe('ActionManager', () => {
-    let server: PluginsServer
+    let hub: Hub
     let closeServer: () => Promise<void>
     let actionManager: ActionManager
 
     beforeEach(async () => {
-        ;[server, closeServer] = await createServer()
+        ;[hub, closeServer] = await createHub()
         await resetTestDatabase()
-        actionManager = new ActionManager(server.db)
+        actionManager = new ActionManager(hub.db)
         await actionManager.prepare()
     })
     afterEach(async () => {
@@ -45,7 +45,69 @@ describe('ActionManager', () => {
             ],
         })
 
-        await server.db.postgresQuery(
+        await hub.db.postgresQuery(
+            `UPDATE posthog_actionstep SET properties = jsonb_set(properties, '{0,key}', '"baz"') WHERE id = 911`,
+            undefined,
+            'testKey'
+        )
+
+        // This is normally dispatched by Django and broadcasted by Piscina
+        await actionManager.reloadAction(67)
+        const reloadedAction = actionManager.getAction(67)
+
+        expect(reloadedAction).toMatchObject({
+            id: 67,
+            name: 'Test Action',
+            deleted: false,
+            post_to_slack: false,
+            slack_message_format: '',
+            is_calculating: false,
+            steps: [
+                {
+                    id: 911,
+                    action_id: 67,
+                    tag_name: null,
+                    text: null,
+                    href: null,
+                    selector: null,
+                    url: null,
+                    url_matching: null,
+                    name: null,
+                    event: null,
+                    properties: [{ type: 'event', operator: PropertyOperator.Exact, key: 'baz', value: ['bar'] }],
+                },
+            ],
+        })
+    })
+
+    it('returns the correct action when reloaded via Piscina', async () => {
+        const action = actionManager.getAction(67)
+
+        expect(action).toMatchObject({
+            id: 67,
+            name: 'Test Action',
+            deleted: false,
+            post_to_slack: false,
+            slack_message_format: '',
+            is_calculating: false,
+            steps: [
+                {
+                    id: 911,
+                    action_id: 67,
+                    tag_name: null,
+                    text: null,
+                    href: null,
+                    selector: null,
+                    url: null,
+                    url_matching: null,
+                    name: null,
+                    event: null,
+                    properties: [{ type: 'event', operator: PropertyOperator.Exact, key: 'foo', value: ['bar'] }],
+                },
+            ],
+        })
+
+        await hub.db.postgresQuery(
             `UPDATE posthog_actionstep SET properties = jsonb_set(properties, '{0,key}', '"baz"') WHERE id = 911`,
             undefined,
             'testKey'
