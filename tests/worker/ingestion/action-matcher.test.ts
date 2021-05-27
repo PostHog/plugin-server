a957179912ef31941d08f18585c692bb9e21197c
Michael Matloka <dev@twixes.com>
2021-05-27 13:15:05 +0200

    Add a legit ActionMatcher test

--- tests/worker/ingestion/action-matcher.test.ts
+++ tests/worker/ingestion/action-matcher.test.ts
@@ -1,24 +1,11 @@
 import { PluginEvent } from '@posthog/plugin-scaffold'
 
-import { Hub, PropertyOperator } from '../../../src/types'
+import { Action, ActionStepUrlMatching, Hub, PropertyOperator } from '../../../src/types'
 import { createHub } from '../../../src/utils/db/hub'
-import { ActionManager } from '../../../src/worker/ingestion/action-manager'
 import { ActionMatcher } from '../../../src/worker/ingestion/action-matcher'
 import { resetTestDatabase } from '../../helpers/sql'
 
-function createEvent(index = 0): PluginEvent {
-    return {
-        distinct_id: 'my_id',
-        ip: '127.0.0.1',
-        site_url: 'http://localhost',
-        team_id: 2,
-        now: new Date().toISOString(),
-        event: 'default event',
-        properties: { key: 'value', index },
-    }
-}
-
-describe('ActionManager', () => {
+describe('ActionMatcher', () => {
     let hub: Hub
     let closeServer: () => Promise<void>
     let actionMatcher: ActionMatcher
@@ -38,9 +25,85 @@ describe('ActionManager', () => {
     const ACTION_ID = 69
     const ACTION_STEP_ID = 913
 
-    it('returns the correct actions generally', async () => {
-        const actionsMatched = await actionMatcher.match(createEvent())
+    it('returns no match if action has no steps', () => {
+        const event: PluginEvent = {
+            distinct_id: 'my_id',
+            ip: '127.0.0.1',
+            site_url: 'http://localhost',
+            team_id: 2,
+            now: new Date().toISOString(),
+            event: 'default event',
+            properties: { key: 'value' },
+        }
 
-        expect(actionsMatched).toEqual([])
+        const actionDefinition: Action = {
+            id: 1,
+            team_id: 2,
+            name: 'Test',
+            created_at: new Date().toISOString(),
+            created_by_id: 999,
+            deleted: false,
+            post_to_slack: false,
+            slack_message_format: '',
+            is_calculating: false,
+            updated_at: new Date().toISOString(),
+            last_calculated_at: new Date().toISOString(),
+            steps: [],
+        }
+
+        expect(actionMatcher.checkAction(event, actionDefinition)).toBeFalsy()
+    })
+
+    it('returns a match in case of URL contains page view', () => {
+        const eventPosthog: PluginEvent = {
+            distinct_id: 'my_id',
+            ip: '127.0.0.1',
+            site_url: 'http://localhost',
+            team_id: 2,
+            now: new Date().toISOString(),
+            event: '$pageview',
+            properties: { $current_url: 'http://posthog.com/pricing' },
+        }
+        const eventExample: PluginEvent = {
+            distinct_id: 'my_id',
+            ip: '127.0.0.1',
+            site_url: 'http://localhost',
+            team_id: 2,
+            now: new Date().toISOString(),
+            event: '$pageview',
+            properties: { $current_url: 'https://example.com/' },
+        }
+
+        const actionDefinition: Action = {
+            id: 1,
+            team_id: 2,
+            name: 'Test',
+            created_at: new Date().toISOString(),
+            created_by_id: 999,
+            deleted: false,
+            post_to_slack: false,
+            slack_message_format: '',
+            is_calculating: false,
+            updated_at: new Date().toISOString(),
+            last_calculated_at: new Date().toISOString(),
+            steps: [
+                {
+                    id: 5,
+                    action_id: 1,
+                    tag_name: null,
+                    text: null,
+                    href: null,
+                    selector: null,
+                    url: 'example.com',
+                    url_matching: ActionStepUrlMatching.Contains,
+                    name: null,
+                    event: '$pageview',
+                    properties: null,
+                },
+            ],
+        }
+
+        expect(actionMatcher.checkAction(eventPosthog, actionDefinition)).toBeFalsy()
+        expect(actionMatcher.checkAction(eventExample, actionDefinition)).toBeTruthy()
     })
 })
