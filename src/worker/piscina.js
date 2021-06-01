ca20ba61ee344190930328cd529dd8358db445f3
Marius Andra <marius.andra@gmail.com>
2021-06-01 13:44:46 +0200

    run piscina worker in the main thread if concurrency at 1

--- src/worker/piscina.js
+++ src/worker/piscina.js
@@ -6,12 +6,47 @@ if (isMainThread) {
     const { createConfig } = require('./config')
     module.exports = {
         makePiscina: (serverConfig) => {
-            const piscina = new Piscina(createConfig(serverConfig, __filename))
-            piscina.on('error', (error) => {
-                Sentry.captureException(error)
-                console.error('⚠️', 'Piscina worker thread error:\n', error)
-            })
-            return piscina
+            const config = createConfig(serverConfig, __filename)
+            if (config.maxThreads === 1) {
+                const { createWorker } = require('./worker')
+                const workerPromise = createWorker(serverConfig, threadId)
+                const promises = new Set()
+                let completed = 0
+                const awaitWorker = async ({ task, args }) => await (await workerPromise)({ task, args })
+                const trackedWorker = ({ task, args }) => {
+                    const promise = awaitWorker({ task, args })
+                    promises.add(promise)
+                    promise.finally(() => {
+                        promises.delete(promise)
+                        completed++
+                    })
+                    return promise
+                }
+                return {
+                    runTask: ({ task, args }) => trackedWorker({ task, args }),
+                    broadcastTask: ({ task, args }) => trackedWorker({ task, args }),
+                    destroy: () => Promise.all(promises),
+                    on: () => undefined,
+                    options: {},
+                    queueSize: 0,
+                    threads: [true],
+                    completed: completed,
+                    waitTime: 0,
+                    runTime: 0,
+                    utilization: 0,
+                    duration: 0,
+                    isWorkerThread: true,
+                    workerData: null,
+                    version: 'test',
+                }
+            } else {
+                const piscina = new Piscina(config)
+                piscina.on('error', (error) => {
+                    Sentry.captureException(error)
+                    console.error('⚠️', 'Piscina worker thread error:\n', error)
+                })
+                return piscina
+            }
         },
     }
 } else {
