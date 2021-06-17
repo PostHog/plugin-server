f1bc859bc14e08a830516f88124ae7b5f6fdd293
Michael Matloka <dev@twixes.com>
2021-06-17 23:25:26 +0200

    Update queryCounter expectations again

--- tests/shared/process-event.ts
+++ tests/shared/process-event.ts
@@ -235,9 +235,9 @@ export const createProcessEventTests = (
         )
 
         if (database === 'clickhouse') {
-            expect(queryCounter).toBe(12 + 14 /* event & prop definitions */)
+            expect(queryCounter).toBe(10 + 14 /* event & prop definitions */)
         } else if (database === 'postgresql') {
-            expect(queryCounter).toBe(16 + 14 /* event & prop definitions */)
+            expect(queryCounter).toBe(14 + 14 /* event & prop definitions */)
         }
 
         let persons = await hub.db.fetchPersons()
