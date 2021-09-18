745dbd2a0c2550beb5fa7cb62f3dc39e17a403ac
Harry Waye <harry@scalexp.com>
2021-09-18 11:24:08 +0100

    wip

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
     testEnvironment: 'node',
     clearMocks: true,
     coverageProvider: 'v8',
-    setupFilesAfterEnv: ['./jest.setup.fetch-mock.js', './jest.setup.databases.ts'],
+    setupFilesAfterEnv: ['./jest.setup.fetch-mock.js'],
     testMatch: ['<rootDir>/tests/**/*.test.ts', '<rootDir>/benchmarks/**/*.benchmark.ts'],
 }
